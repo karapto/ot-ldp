b20cfb202b61ac9b3568e440b62af97cfccdffee
karapto <ventablack.513.827@gmail.com>
2022-01-02 17:32:05 +0900

    grid_scale code

--- run.sh
+++ run.sh
@@ -1,27 +1,6 @@
-#! /bin/bash -x
-# usage
-#  - script/eval.sh 50006 127.0.0.1 32 1.0 10
-port=$1
-address=$2
-cate_max=$3
-epsilon=$4
-times=$5
 
-g=0
-for width in 100 1000; do
-	for mech in krr oue olh; do
-		for cate_num in $(seq 2 $cate_max); do
-			for i in $(seq 1 $times) ; do
-				if [ $cate_num -le 3 ] ; then
-					g=2
-				else
-					g=$(($cate_num / 2))
-				fi
-				python server.py --mech $mech --cate_num $cate_num --width $width --epsilon $epsilon --port $port --address $address --g $g &
-				sleep 1
-				python client.py --mech $mech --cate_num $cate_num --width $width --epsilon $epsilon --port $port --address $address --sensitive_value 1 --g $g
-			done
-		done
-	done
+for i in {16..96}; do
+    python3 server.py --mech k_rr --cate_num $i --width 100 --epsilon 3.0 --port 50006 --address 127.0.0.1 &
+    sleep 1
+    python3 client.py --mech k_rr --cate_num $i --width 100 --epsilon 3.0 --port 50006 --address 127.0.0.1 --sensitive_value 9
 done
-
