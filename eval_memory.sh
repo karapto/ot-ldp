9f4fb27735142270d41899b2ebd7e00ab668e517
karapto <ventablack.513.827@gmail.com>
2022-01-14 18:24:54 +0900

    add sleep time

--- eval_memory.sh
+++ eval_memory.sh
@@ -2,6 +2,7 @@ array=(16 100)
 for i in {1..10}; do
     for j in ${array[@]}; do
         python3 server.py --mech k_rr --cate_num $j --width 100 --epsilon $i --port 50006 --address 127.0.0.1 &
+        sleep 10
         python3 client.py --mech k_rr --cate_num $j --width 100 --epsilon $i --port 50006 --address 127.0.0.1 --sensitive_value $(( $RANDOM % $i ))
         sleep 10
     done
