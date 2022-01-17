5355dbd90699af4419a1b38ac163f531e472a82a
karapto <ventablack.513.827@gmail.com>
2022-01-17 10:02:34 +0900

    add tcp100to10000.sh

---
+++ run_tcp1000.sh
@@ -0,0 +1,10 @@
+array_scale=(4 16 63 250)
+
+for i in {1..10}; do
+        for j in ${array_scale[@]}; do
+                python3 server.py --mech k_rr --cate_num $j --width 1000 --epsilon $i --port 50006 --address 127.0.0.1 &
+                sleep 10
+                python3 client.py --mech k_rr --cate_num $j --width 1000 --epsilon $i --port 50006 --address 127.0.0.1 --sensitive_value $(( $RANDOM % $i ))
+                sleep 10
+        done
+done
