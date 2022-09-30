69e82356179151b937a87a7faa8345105ff58879
karapto <ventablack.513.827@gmail.com>
2022-09-30 16:22:20 +0900

    add runscript

---
+++ acc_srv.sh
@@ -0,0 +1,8 @@
+array_scale={50006..50106}
+
+for i in {1..10}; do
+        for j in ${array_scale[@]}; do
+                python3 server.py --mech k_rr --cate_num 16 --width 1000 --epsilon $i --port 50006 --address 127.0.0.1 &
+        done
+done
+
