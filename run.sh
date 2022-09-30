9535dd3416da67342d5b45319f6ca28f78a48bfa
karapto <39578490+karapto@users.noreply.github.com>
2022-09-30 11:43:40 +0900

    Delete run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-
-for i in {16..96}; do
-    python3 server.py --mech k_rr --cate_num $i --width 100 --epsilon 3.0 --port 50006 --address 127.0.0.1 &
-    sleep 1
-    python3 client.py --mech k_rr --cate_num $i --width 100 --epsilon 3.0 --port 50006 --address 127.0.0.1 --sensitive_value 9
-done
