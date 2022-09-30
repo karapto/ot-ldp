57181285183a9ceddc18d3d846e35e2c33ff6a90
karapto <39578490+karapto@users.noreply.github.com>
2022-09-30 11:43:58 +0900

    Delete eval_runtime.sh

--- eval_runtime.sh
+++
@@ -1,8 +0,0 @@
-array=(16 100)
-
-for i in ${array[@]}; do
-    python3 server.py --mech k_rr --cate_num $i --width 100 --epsilon 10.0 --port 50006 --address 127.0.0.1 &
-    python3 client.py --mech k_rr --cate_num $i --width 100 --epsilon 10.0 --port 50006 --address 127.0.0.1 --sensitive_value $(( $RANDOM % $i ))
-done
-
-
